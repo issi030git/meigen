6c943aaf411cb4c3b7445a7e07a31fb251bf3f6f
issi8131 <shochioa@gmail.com>
2025-04-09 20:54:12 +0900

    名言取得失敗時のエラーダイアログ追加

--- js/main.js
+++ js/main.js
@@ -10,6 +10,7 @@ const App = {
       fetchNum: 5,
       dataList: {},
       isDataLoading: false,
+      isLoadFailed: false,
     }
   },
   // dataの更新を受け処理をした結果を返す(引数使用不可)
@@ -28,6 +29,7 @@ const App = {
       return arg1 + arg2;
     },
     fetchMeigen() {
+      this.isLoadFailed = false;
       this.isDataLoading = true;
       //以下非同期処理でURLアクセス(GETパラメータにタイムスタンプを付加し同URLへのブラウザキャッシュが効かないようにしている)
       axios.get('https://api.allorigins.win/get?url=' +
@@ -35,12 +37,15 @@ const App = {
       ).then(response => {//取得成功したら
         this.dataList = JSON.parse(response.data.contents);
       }).catch(error => {
-        console.error("Error:", error);
+        this.isLoadFailed = true;
+        ui("#load-failed-dialog");
+        // console.error("Error:", error);
       }).finally(() => {
         this.isDataLoading = false;
       });
     },
     fetchMeigenD() {
+      this.isLoadFailed = false;
       this.isDataLoading = true;
       this.dataList = JSON.parse('[{"meigen":"真の友は共に孤独である。","auther":"ボナール"},{"meigen":"解決策が分らないのではない。問題が分っていないのだ","auther":"チェスタートン"},{"meigen":"心の奥底に達してあらゆる病を癒せる音楽、それは暖かい言葉だ。","auther":"エマーソン"},{"meigen":"平等主義者は彼ら自身の水準まで他人を引き上げることを望むが、彼ら自身以上に引き上げようとはしない。","auther":"サミュエル＝ジョンソン"},{"meigen":"生きる勇気を持たないものは、戦う前に消えていく","auther":"浦沢直樹、勝鹿北星(漫画家) 出典:MASTERキートン キャラクタ:平賀・キートン・太一 [漫画・アニメの名言]"}]');
       this.isDataLoading = false;
